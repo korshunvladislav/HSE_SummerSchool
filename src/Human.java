6f7b158e5facc031b8d164010da3836afd673310
Vladislav <vladislav.korshun.2005@mail.ru>
2023-07-03 15:10:03 +0300

    Commit message

--- src/Human.java
+++ src/Human.java
@@ -11,6 +11,8 @@ public class Human {
 
     public static void main(String[] args) {
         Human elon = new Human("Elon", "Musk", 52);
-        System.out.println("Human 2 с именем " + elon.name + " " + elon.surname);
+        Human vlad = new Human("Vladislav", "Korshun", 18);
+        System.out.println("Human 1 с именем " + elon.name + " " + elon.surname);
+        System.out.println("Human 2 с именем " + vlad.name + " " + vlad.surname);
     }
 }
